0f4580ba16e426af8fa247439eb7130870b59624
Publoz <publoz123@gmail.com>
2021-11-04 23:39:45 +1300

    Adding in file reader for weekly results and template files

--- draft.py
+++ draft.py
@@ -1,3 +1,4 @@
+from os import name
 import sqlite3
 from sqlite3 import Error
 
@@ -49,6 +50,21 @@ def create_tables():
 	);
     """
 
+    tmp_results = """
+    CREATE TABLE IF NOT EXISTS TmpResults (
+        playerName TEXT,
+        club TEXT,
+        year INTEGER,
+		round INTEGER,
+		goals INTEGER,
+		twoMins INTEGER,
+		win INTEGER NOT NULL,
+		points INTEGER,
+		PRIMARY KEY(playerName, club, year, round),
+		FOREIGN KEY(playerName, club, year) REFERENCES Players(playerName, club, year)
+	);
+    """
+
     gms = """
     CREATE TABLE IF NOT EXISTS Gms (
         playerName TEXT,
@@ -63,6 +79,7 @@ def create_tables():
     """ 
     execute_query(connection, players)
     execute_query(connection, results)
+    execute_query(connection, tmp_results)
     execute_query(connection, gms)
 
 def update_results():
@@ -72,6 +89,63 @@ def update_results():
     """
     execute_query(connection, query)
 
+def load_file_results(path):
+    file = open(path, "r")
+    year = int(file.read(4))
+    file.read(1)
+    round = int(file.readline().strip('\n'))
+
+    winners = set()
+    line = file.readline() # <winners>
+    line = file.readline() #first winners
+    while(line.strip('\n') != "<\Winners>"):
+        winners.add(line.strip('\n'))
+        line = file.readline()
+
+    
+    while(True):
+        club = file.readline().strip('\n')
+        team = file.readline().strip('\n')
+        win = 0
+        if( (club + team) in winners):
+            win = 1
+        
+        line = file.readline()
+
+        #Loop through team
+        while(line != team[:1] + '\\' + team[1:]):
+            chunks = line.split()
+            name = ""
+            goals = 0
+            two_mins = 0
+            for i in range(len(chunks)):
+                if(chunks[i].isdigit()):
+                    goals = chunks[i]
+                    if(i == len(chunks)-1):
+                        break
+                    else:
+                        two_mins = chunks[i+1]
+                        break
+                else:
+                    name += chunks[i]
+
+            query = """
+                INSERT INTO TmpResults
+                VALUES ({}, {}, {}, {}, {}, {}, {}, NULL)
+            """.format(name, club, year, round, goals, two_mins, win)
+            line = file.readline()
+        #assert file.readline() == 
+    
+
+                
+
+
+
+
+
+    file.close()
+
 
 #--------------------------------START----------------------------------------------
 connection = create_connection("fantasyDatabase.sqlite")
+#load_file_results("exFile.txt")
